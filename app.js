b90413d5dd10addcb424738974fe556f70f7f6bd
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2021-11-11 16:00:41 -0500

    simpler

--- app.js
+++ app.js
@@ -3,30 +3,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-
 app.get('/one', (req, res) => {
-  return res.send(`<body><script>
-    const form = document.createElement('form');
-    form.method = 'POST';
-    document.body.appendChild(form);
-    form.submit();
-    </script></body>`)
-})
-
-app.post('/one', (req, res) => {
-  setTimeout(() => {
-    return res.redirect('/two');
-  }, 2000)
-})
-
-app.get('/two', (req, res) => {
-  setTimeout(() => {
-  return res.send(`<body>
-    hello world
-  </body>`)
-
-  }, 1000)
-
+  return res.send(`<body>hello there</body>`)
 })
 
 app.listen(port);
